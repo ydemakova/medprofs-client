bf7e11e6b99aa13c42802519f4cc72065159ce59
Yulia Demakova <j.demakova@gmail.com>
2021-12-13 20:38:06 +0100

    add delete article

--- src/components/article/ArticleListPage.js
+++ src/components/article/ArticleListPage.js
@@ -1,21 +1,29 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-// import { API_URL } from '../../'
+import { API_URL } from '../../config'
 
 export default function ArticleListPage() {
 	const [articles, setArticles] = useState([])
 
 	useEffect(() => {
 		;(async () => {
-			let res = await axios.get(`http://localhost:5005/api/articles`)
+			const res = await axios.get(`${API_URL}/articles`)
 			setArticles(res.data)
 		})()
 	}, [])
 
+	async function deleteArticle(id) {
+		await axios.delete(`${API_URL}/articles/${id}`)
+		const filteredArticles = articles.filter((elem) => {
+			return elem._id !== id
+		})
+		setArticles(filteredArticles)
+	}
+
 	return (
 		<div className="articles">
-			<div className="display-flex">
+			<div className="d-flex justify-content-between align-items-baseline">
 				<h1>Article list page</h1>
 				<Link to="/articles/new" className="btn btn-primary">
 					New Article
@@ -36,15 +44,23 @@ export default function ArticleListPage() {
 				</thead>
 				<tbody>
 					{articles.map((article, index) => (
-						<tr key={article._id}>
-							<td>{index}.</td>
+						<tr key={article._id} className="align-middle">
+							<td>{index + 1}.</td>
 							<td>{article.title}</td>
 							<td>{article.tags}</td>
 							<td>{article.locations}</td>
 							<td>{article.text}</td>
 							<td>{article.author}</td>
 							<td>{article.type}</td>
-							<td>edit | delete</td>
+							<td>
+								<Link className="btn btn-primary" to="/articles/:id">
+									edit
+								</Link>
+								&nbsp;
+								<button className="btn btn-danger" onClick={() => deleteArticle(article._id)}>
+									delete
+								</button>
+							</td>
 						</tr>
 					))}
 				</tbody>
