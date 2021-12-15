cde87d2c8daa3e3d51c921e87e8ff6d60fa36b1b
Yulia Demakova <j.demakova@gmail.com>
2021-12-15 23:37:21 +0100

    update homepage

--- src/components/misc/HomePage.js
+++ src/components/misc/HomePage.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { API_URL } from '../../config'
 
 export default function HomePage() {
+	const [articles, setArticles] = useState([])
+	useEffect(() => {
+		;(async () => {
+			const res = await axios.get(`${API_URL}/articles?isNewest=true`)
+			setArticles(res.data)
+		})()
+	}, [])
+
 	return (
 		<div>
-			<h1>Welcome</h1>
+			<h2 className="text-center">Newest Articles</h2>
+			<div className="all-articles container">
+				<div className="row">
+					{articles.map((article) => (
+						<div key={article._id} className="col-4">
+							<div className="card mb-3">
+								<div className="card-body">
+									<h5 className="card-title">{article.title}</h5>
+									<p className="card-author">Author: {article.author?.username}</p>
+									<Link className="btn btn-primary" to={'/articles/' + article._id}>
+										Read more ...
+									</Link>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
 		</div>
 	)
 }
